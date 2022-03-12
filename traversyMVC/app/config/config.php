75488133be65dfe8cfe2f434d166a8f97cf541fe
ahmadaldabouqii <aldabouqiahmad@gmail.com>
2022-03-12 23:00:51 +0300

    MVC Built

---
+++ traversyMVC/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'tmvc');
+
+// App Root
+define('APPROOT', dirname(__FILE__,2));
+// URL Root
+define('URLROOT','http://localhost/php_projects/php_toturial/traversyMVC');
+// Site Name
+define('SITENAME','AhmadMVC');
